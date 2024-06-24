66173b833e6f44a3f17acaa2d0eacb7f90497c6a
soukanalytics <admin@soukanalytics.com>
2024-06-24 19:20:17 -0300

    pequeno ajuste

--- descricao.R
+++ descricao.R
@@ -12,9 +12,9 @@ library(xlsx)
 library(tidyverse)
 
 # Bases --------------------------------------------------------------
-plano_de_contas <- read_excel("bases/plano_de_contas.xlsx")
-contas <- read_excel("bases/visao_competencia_2024_05.xlsx")
-
+plano_de_contas <- read_excel("bases/Plano de Contas Piccolo.v.002.xlsx")
+#contas <- read_excel("bases/visao_competencia_2024_05.xlsx")
+contas <- read_excel("bases/visao_competencia_2024_07_pre.xls")
 
 # Analise -----------------------------------------------------------------
 contas$cod_flux_op <- substr(contas$`Categoria 1`, 1, 7) 
@@ -24,5 +24,9 @@ analise <- analise %>%
 	group_by(fluxo_op, tipo_1,tipo_2) %>%
 	summarise(valor = sum(`Valor (R$)`, na.rm = T))
 analise <- subset(analise, valor < 0)
+
+analise2 <- analise %>%
+	group_by(tipo_2) %>%
+	summarise(valor = sum(valor, na.rm = T))
 
 write.csv2(analise, 'analise_maio.csv')
